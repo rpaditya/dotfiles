e37635cc6788a88eedda7d3a10262299eaf08ff9
R.P. (Adi) Aditya <aditya@grot.org>
2015-02-13 23:43:40 -0500

    cleanup

--- bin/mailrun.sh
+++ bin/mailrun.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+cleanup() {
+    /usr/bin/sqlite3 ~/.offlineimap/Account-mswork/LocalStatus-sqlite/INBOX 'pragma integrity_check;'
+}
+
 # Check every ten seconds if the process identified as $1 is still 
 # running. After 5 checks (~60 seconds), kill it. Return non-zero to 
 # indicate something was killed.
@@ -7,13 +12,14 @@ monitor() {
   local pid=$1 i=0
 
   while ps $pid &>/dev/null; do
-    if (( i++ > 5 )); then
+    if (( i++ > 12 )); then
       logger -p crit "Max checks reached. Sending SIGKILL to ${pid}..."
 #      echo "Max checks reached. Sending SIGKILL to ${pid}..." >&2
       kill -9 $pid; return 1
     fi
 
     sleep 10
+#    cleanup
   done
 
   return 0
@@ -28,6 +34,10 @@ if ps $pid &>/dev/null; then
 fi
 
 logger -p info "Starting offlineimap"
-offlineimap -o -u quiet & monitor $!
+out=`offlineimap -o -u quiet & monitor $!`
+logger -p info "${out}"
+#if [ $? -ne 0 ]; then
+#    cleanup
+#fi
 logger -p info "end of mailrun.sh/offlineimap script"
 
