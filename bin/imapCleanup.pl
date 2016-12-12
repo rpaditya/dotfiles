6e8156468f00e8bcbc440b0793fbe22097c8429a
R.P. (Adi) Aditya <aditya@grot.org>
2016-12-12 01:34:50 -0500

    -a

---
+++ bin/imapCleanup.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+use strict;
+local $| = 1;
+
+#send folders in comma separated 
+my($repo, $fldrs) = @ARGV;
+
+my($USER) = $ENV{'USER'};
+
+#print $repo . "|" . $fldrs . "|\n";
+
+if (defined $fldrs && $fldrs ne ""){
+    my(@folders) = split(',', $fldrs);
+
+    for my $f (@folders){
+	chomp $f;
+	`logger -p info "${USER} rebuilding due to invalid imap folder uid: ${f}"`;
+	print `rm -fr ~/.offlineimap/Account-${repo}/LocalStatus/${f}`;
+	print `rm -fr ~/.offlineimap/Repository-${repo}/FolderValidity/${f}`;
+    }
+
+    print `offlineimap -o -u ttyui -a ${repo} -f ${fldrs}`;
+}
